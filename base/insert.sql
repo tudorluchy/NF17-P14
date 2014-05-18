56f44439409f38dba65314c10cca0607e4de7e4b
Tudor Luchiancenco <tudorluchy@gmail.com>
2014-05-18 17:28:38 +0200

    avancement administration

---
+++ base/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO tPersonne (telephone, nom, prenom) VALUES (0611111111, 'Marcel', 'Silvio');
+INSERT INTO tPersonne (telephone, nom, prenom) VALUES (0622222222, 'Liviu', 'Klein');
+INSERT INTO tPersonne (telephone, nom, prenom) VALUES (0633333333, 'Lokjgk', 'Moiygfd');
+INSERT INTO tPersonne (telephone, nom, prenom) VALUES (0644444444, 'Poubtrz', 'Ngrzzz');
+INSERT INTO tPersonne (telephone, nom, prenom) VALUES (0655555555, 'Seiulttt', 'Vuouzb');
+INSERT INTO tPersonne (telephone, nom, prenom) VALUES (0666666666, 'Gouqnau', 'Nbuzozy');
+
+INSERT INTO tPersonne (telephone, nom, prenom) VALUES (0699999999, 'admin', 'admin');
+
+INSERT INTO tVeterinaire (telephone) VALUES (0633333333);
+INSERT INTO tVeterinaire (telephone) VALUES (0644444444);
+INSERT INTO tEmploye (telephone) VALUES (0655555555);
+INSERT INTO tEmploye (telephone) VALUES (0666666666);
