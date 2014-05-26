a2d32b4933c5a46ae98179e826b8114560dd73d7
clemenceLO21 <clemence.geffray@gmail.com>
2014-05-26 19:56:36 +0200

    Qqs modifs pour classe RDV + module ajouter rdv mais pas testées car pb vpn

--- modules/Employe/ajout_rdv.php
+++ modules/Employe/ajout_rdv.php
@@ -1,6 +1,7 @@
-<h1>Ajout d'un rendez-vous</h1>
 
-<form  enctype="multipart/form-data" name="inscription" action="?module=Employe&action=validation_rdv" method="POST">
+	<h1>"Ajout d'un rendez-vous"</h1>
+
+<form  enctype="multipart/form-data" name="rdv" action="?module=Employe&action=validation_rdv" method="POST">
 	<fieldset>
 		<label for="date_rdv">Date du rendez-vous :</label>
 		<input name="date_rdv" type="text" id="date_rdv">
@@ -10,6 +11,32 @@
 		
 		<label for="tel_prop">Téléphone du propriétaire</label>
 		<input name="tel_prop" type="text" id="tel_prop">
+		
+		<label for="num_dossier">Numéro de dossier</label>
+		<input name="num_dossier" type="text" id="num_dossier">
+		
+		<label>Espèce</label>
+		<select id='espece' name='espece'>
+		<?php 
+		foreach ($liste_especes as $espece) {
+		?>
+			<option value=<?php echo "'".$espece['nom']."'"; ?> <?php // echo "selected=selected"; ?> ><?php echo $espece['nom']; ?></option>
+		<?php 
+		}
+		?>
+		</select>
+		
+		<label>Race</label>
+		<select id='race' name='race'>
+		<?php 
+		foreach ($liste_race as $race) {
+		?>
+			<option value=<?php echo "'".$race['nom']."'"; ?> <?php // echo "selected=selected"; ?> ><?php echo $race['nom']; ?></option>
+		<?php 
+		}
+		?>
+		</select>
+		
 		<div class="bloc_inscrip">
 			<input  type="reset" name="reset" value="Reset"/>
 			<input  type="submit" name="valider" value="Valider"/>	
