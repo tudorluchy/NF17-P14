58bb402f49e9a5ff1b0f1cad32ffc4ad96457a3a
clemenceLO21 <clemence.geffray@gmail.com>
2014-06-04 09:56:06 +0200

    Modification de la présentation de l'équipe

--- modules/default/equipe.php
+++ modules/default/equipe.php
@@ -1,70 +1,40 @@
 <h1>L'équipe</h1>
 
-<h2>Noëlle Cochet-Faivre - Docteur Vétérinaire </h2>
+<h2>Tudor Luchiancenco - Chef de projet</h2>
 
-<img src="template/noelle.jpg" width="120px"/>
+<img src="template/Tudor.jpg" width="120px"/>
 <ul>
-    <li>Diplômée de l'Ecole vétérinaire d'Alfort</li>
-    <li>Spécialiste de Dermatologie Vétérinaire</li>
-    <li>Praticien hospitalier, service de Dermatologie de l'Ecole Vétérinaire d'Alfort</li>
-    <li>Ancien Professeur invitée de l'Université de Mexico (UNAM)</li>
-    <li>Diplômée de l'European College of Veterinary Dermatology (ECVD)</li>
-    <li>DU de Statistiques appliquées à la recherche clinique</li>
-    <li>CES d'Immunologie Animale et Comparée</li>
-    <li>DEA de bases fondamentales de l'oncogénèse (Institut Gustave Roussy)</li>
+    <li>Etudiant à l'UTC</li>
+    <li>GI05</li>
+    <li>Filière SRI</li>
+    <li>Diplômé d'un DUT d'Informatique (juin 2011)</li>
 </ul>
 
-<h2>Hugues Gaillot - Docteur Vétérinaire</h2>
+<h2>Benoît Sénéchal - Responsable conception</h2>
 
-<img src="template/gaillot.jpg" width="120px"/>
+<img src="template/Benoit.jpg" width="120px"/>
 <ul>
-    <li>Diplômé de l'Ecole vétérinaire d'Alfort</li>
-    <li>Spécialiste d'Imagerie Médicale Vétérinaire</li>
-    <li>Diplômé de l'Institut Pasteur en Biologie Moléculaire et Cellulaire</li>
-    <li>DEA de Génétique Humaine (Paris 6)</li>
-    <li>Ancien interne de Médecine interne à l'Ecole Vétérinaire d'Alfort</li>
-    <li>Ancien chargé de consultation de Médecine de l'Ecole Vétérinaire d'Alfort</li>
-    <li>Diplômé de l'European College of Veterinary Diagnostic Imaging (ECVDI)</li>
+    <li>Etudiant Apprenti à l'UTC</li>
+    <li>GI02</li>
+    <li>Diplômé de l'IUT d'Informatique d'Amiens (juin 2013)</li>
+    <li>Organisateur du Hackathon</li>
 </ul>
 	
-<h2>Kirsten Gnirs - Docteur Vétérinaire</h2>
+<h2>Thibault Brocheton - Responsable développement</h2>
 
-<img src="template/gnirs.jpg" width="120px"/>
+<img src="template/Thibault.jpg" width="120px"/>
 <ul>
-    <li>Diplômée de l’Ecole vétérinaire d’Alfort</li>
-    <li>Ancienne Interne de médecine interne à l’Ecole vétérinaire d’Alfort</li>
-    <li>Ancienne Résidente en neurologie-neurochirurgie à l’Ecole vétérinaire d’Alfort</li>
-    <li>DU de Myologie Humaine (Hôpital Pitié-Salepétrière)</li>
-    <li>Diplômé de l’European College of Veterinary Neurology (ECVN)</li>
+    <li>Etudiant à l'UTC</li>
+    <li>GI02</li>
+    <li>Chefetain chez les Scouts et Guides de France</li>
+	<li>Président de Lan'UTC</li>
 </ul>
 
-<h2>Leslie Guillon - Docteur Vétérinaire</h2>
+<h2>Clémence Geffray - Responsable qualité</h2>
 
-<img src="template/guillon.jpg" width="120px"/>
+<img src="template/Clemence.jpg" width="120px"/>
 <ul>
-    <li>Diplômée de la Faculté de Médecine Vétérinaire de Liège</li>
-    <li>DU de microchirurgie</li>
-</ul>
-
-<h2>Philippe Hennet - Docteur Vétérinaire</h2>
-
-<img src="template/hennet.jpg" width="120px"/>
-<ul>
-    <li>Diplômé de l’Ecole vétérinaire de Toulouse</li>
-    <li>Ancien Résident en dentisterie à Hôpital de l’Université de Pennsylvanie</li>
-    <li>Diplômé de l’American Veterinary Dental College (AVDC)</li>
-    <li>Diplômé de l’European Veterinary Dental College (EVDC)</li>
-</ul>
-
-<h2>Pascal Prélaud - Docteur Vétérinaire</h2>
-
-<img src="template/prelaud.jpg" width="100px"/>
-<ul>
-    <li>Diplômé de l’Ecole vétérinaire de Toulouse</li>
-    <li>Spécialiste de Dermatologie vétérinaire</li>
-    <li>DESV de Dermatologie Vétérinaire</li>
-    <li>CES de Dermatologie vétérinaire</li>
-    <li>CES d’Immunologie Animale et Comparée</li>
-    <li>DU de statistiques appliquées à recherche clinique </li>
-    <li>Diplômé de l’European College of Veterinary Dermatology (ECVD)</li>
+    <li>Etudiante Apprentie à l'UTC</li>
+    <li>GI02</li> 
+	<li>Membre d'Ignénieurs Sans Frontière</li>
 </ul>
